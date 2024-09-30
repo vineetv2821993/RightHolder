dbaccfdf13034976aedbc6c0d334839d52c8324f
Jasmeet Singh <jasmeet.singh@bytescare.com>
2024-09-30 11:23:54 +0000

    added count data

--- pages/Login/jsobjects/login/login.js
+++ pages/Login/jsobjects/login/login.js
@@ -1,4 +1,5 @@
 export default {
+	exitQuery:'',
 	signInQuery: '',
 	async checkExpireUser(loginId) {
 		// Fetch the expire_at value from the database
@@ -27,37 +28,47 @@ export default {
 			showAlert("Password is required", "error");
 		}
 		else{
-			const hashedPassword = CryptoJS.SHA256(Input2.text).toString(); // Use the same hash method as used for registration
+			this.exitQuery = `SELECT id FROM taoq_research.rightHolder WHERE email = '${Input1.text}' or username ='${Input1.text}'`;
 
-			this.signInQuery = `
+			// Run the exit query to check if the user exists
+			const result = await ExitRightHolder.run();
+			if (result.length > 0) {
+				const hashedPassword = CryptoJS.SHA256(Input2.text).toString(); // Use the same hash method as used for registration
+
+				this.signInQuery = `
     SELECT id, username, email
     FROM taoq_research.rightHolder
     WHERE (email = '${Input1.text}' OR username = '${Input1.text}')
     AND password_hash = '${hashedPassword}'
 `;
-			console.log(	this.signInQuery );
-			let data = await signInRightHolder.run();
-			if(data && data.length >0){
-				let checkData = await checkRightHolderInfoExit.run({id:data[0].id});
-				let isExpire = await this.checkExpireUser(data[0].id);
-				if(!isExpire){
-					await updateExpireDate.run({id:data[0].id});
+				console.log(	this.signInQuery );
+				let data = await signInRightHolder.run();
+				if(data && data.length >0){
+					let checkData = await checkRightHolderInfoExit.run({id:data[0].id});
+					let isExpire = await this.checkExpireUser(data[0].id);
+					if(!isExpire){
+						await updateExpireDate.run({id:data[0].id});
+					}
+					if(checkData && checkData.length>0){
+						navigateTo('Complaints', {}, 'SAME_WINDOW');
+					}
+					else{
+						navigateTo('Registeration', {}, 'SAME_WINDOW');
+					}
+					storeValue("rightHolderUserId",data[0].id);
+					showAlert("login successfully","info");
 				}
-				if(checkData && checkData.length>0){
-					navigateTo('Complaints', {}, 'SAME_WINDOW');
-				}
-				else{
-					navigateTo('Registeration', {}, 'SAME_WINDOW');
-				}
-				storeValue("rightHolderUserId",data[0].id);
-				showAlert("login successfully","info");
-			}
 
-			else
-			{
-				showAlert("invalid username or email Address and password","error");
+				else
+				{
+					showAlert("invalid username or email Address and password","error");
+				}
+			}
+			else{
+				showAlert("User or email not found, please create the account","warning");
 			}
 		}
+
 
 	}
 }
